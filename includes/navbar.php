31775e7604493426b534b171fe9dc9c434c30d3e
drashti-bhatia <bhatiagrishti@gmail.com>
2025-02-12 20:31:39 +0530

    Add files via upload

---
+++ includes/navbar.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="../assets/style.css">
+
+<div class="navbar">
+    <a href="../home.php">Home</a>
+    <a href="../client/display_product.php">Products</a>
+    <a href="../auth/register.php">Register</a>
+    <a href="../auth/login.php">Login</a>
+</div>
